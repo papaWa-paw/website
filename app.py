d35f3f6acf98ee3e30cd8a2193b1cdfe172b9c15
papaWa-paw <150381694+papaWa-paw@users.noreply.github.com>
2024-03-15 15:43:46 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+from data import db_session
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+if __name__ == '__main__':
+    db_session.global_init("db/blogs.db")
+    app.run(debug=True)
