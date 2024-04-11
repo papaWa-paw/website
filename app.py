893c88aeeb820127978419a9dc645e17b845dedb
papaWa-paw <150381694+papaWa-paw@users.noreply.github.com>
2024-04-11 21:09:38 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from data import db_session
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -15,9 +16,22 @@ def about():
     return render_template('about.html')
 
 
+@app.route('/registration', methods=['POST', 'GET'])
+def registration():
+    if request.method == 'POST':
+        pass
+    else:
+        return render_template('registration.html')
+
+
 @app.route('/sign_in')
 def sign_in():
     return render_template('sign_in.html')
+
+
+@app.route('/recipes_form')
+def recipes_form():
+    return render_template('recipes_form.html')
 
 
 if __name__ == '__main__':
