7ec81ae4c8c0c77696bc8bab75a6d630fd4bf270
papaWa-paw <150381694+papaWa-paw@users.noreply.github.com>
2024-04-12 22:08:08 +0300

    Update users.py

--- data/users.py
+++ data/users.py
@@ -1,10 +1,13 @@
 import datetime
 import sqlalchemy
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
 from .db_session import SqlAlchemyBase
 from sqlalchemy import orm
 
 
-class User(SqlAlchemyBase):
+class User(SqlAlchemyBase, UserMixin):
     __tablename__ = 'users'
 
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
@@ -14,3 +17,9 @@ class User(SqlAlchemyBase):
     created_date = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now)
 
     recipe = orm.relationship("Recipes", back_populates='user')
+
+    def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
