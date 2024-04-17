0eb592368a509a49e758ead95e4b6d3256c37ee4
papaWa-paw <150381694+papaWa-paw@users.noreply.github.com>
2024-04-17 22:53:27 +0300

    Create resform.py

---
+++ forms/resform.py
@@ -0,0 +1,15 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileAllowed
+from wtforms import StringField, TextAreaField, FileField
+from wtforms import SubmitField
+from wtforms.validators import DataRequired
+
+
+class RecipesForm(FlaskForm):
+    title = StringField(validators=[DataRequired()])
+    description = TextAreaField()
+    ingredients = TextAreaField()
+    cooking = TextAreaField()
+    type = StringField(validators=[DataRequired()])
+    picture = FileField(validators=[FileAllowed(['png', 'jpg'])])
+    submit = SubmitField()
