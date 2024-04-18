0bcedd69396324476839ca7ae68556566f15ca54
papaWa-paw <150381694+papaWa-paw@users.noreply.github.com>
2024-04-18 16:53:02 +0300

    Update resipes.py

--- data/resipes.py
+++ data/resipes.py
@@ -1,19 +1,19 @@
 import datetime
 import sqlalchemy
 from sqlalchemy import orm
-
 from .db_session import SqlAlchemyBase
 
 
 class Recipes(SqlAlchemyBase):
-    __tablename__ = 'recipe'
+    __tablename__ = 'recipes'
 
-    id = sqlalchemy.Column(sqlalchemy.Integer,
-                           primary_key=True, autoincrement=True)
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
     title = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    content = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    description = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    ingredients = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    cooking = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    type = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     created_date = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now)
-    is_private = sqlalchemy.Column(sqlalchemy.Boolean, default=True)
 
     user_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("users.id"))
     user = orm.relationship('User')
