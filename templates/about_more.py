2c9e9795e926e672db52df6fda5b570541705826
papaWa-paw <150381694+papaWa-paw@users.noreply.github.com>
2024-04-26 23:22:56 +0300

    Update about_more.py

--- templates/about_more.py
+++ templates/about_more.py
@@ -6,29 +6,35 @@ GG
 
 {% block body %}
 <div>
-  <h3 class="title">{{ recipe.title }}</h3>
-  <td width="60%">
-	  <div id="ib_s_e_3" class="ib_s_e in_seen" data-in_c_id="ingridients" data-in_view_el="3"></div>
-	  <table class="ingr" align="center">
-		  <tbody><tr>
-			  <td colspan="3" class="padding_l ingr_title">
-				  <span class="prod">Продукты </span></td>
-		  </tr>
+  <h2 class="title_more">{{ recipe.title }}</h2>
+	<div class="author">
+		<h1>{{ author.name }}</h1>
+	</div>
+	<div class="ingredient">
+	  <table class="ingr">
+		  <span class="prod"><h4><em>Продукты</em></h4></span>
+		  <hr class="my-4" width="200px">
+		  <div class="ingr">
+		  {%  for i in recipe.ingredients.split('\r\n') %}
 		  <tr class="ingr_tr_0">
-			  <td colspan="3" class="padding_l padding_r"><span class="">Лаваш тонкий - 1 лист</span></td>
-		  </tr><tr class="ingr_tr_1">
-			  <td colspan="3" class="padding_l padding_r"><span class="">Сыр (какой вам нравится) - 150 г</span></td>
-		  </tr><tr class="ingr_tr_0">
-			  <td colspan="3" class="padding_l padding_r"><span class="">Чеснок - 0,5-1 зубчик</span></td>
-		  </tr><tr class="ingr_tr_1">
-			  <td colspan="3" class="padding_l padding_r"><span class="">Зелень свежая, измельченная - 1-2 ст. ложки</span></td>
-		  </tr><tr class="ingr_tr_0">
-			  <td colspan="3" class="padding_l padding_r"><span class="">Яйцо - 1 шт.</span></td>
-		  </tr><tr class="ingr_tr_1">
-			  <td colspan="3" class="padding_l padding_r"><span class="">Сухари панировочные</span></td>
-		  </tr><tr class="ingr_tr_0">
-			  <td colspan="3" class="padding_l padding_r"><span class="">Масло растительное - для жарки</span></td>
-		  </tr></tbody></table>
-  </td>
+			  <td colspan="3" class="padding_l padding_r"><span class="">{{ i }}</span></td>
+		  {% endfor %}
+		  </div>
+	  </table>
+	</div>
+	<div class="description">
+		{{ recipe.description }}
+	</div>
+
+	<div class="cooking">
+		<h4><em>Процесс готовки</em></h4>
+		<hr class="my-4" width="200px">
+		<div class="cook">
+		  {%  for r in recipe.cooking.split('\r\n') %}
+		  <tr class="ingr_tr_0">
+			  <td colspan="3" class="padding_l padding_r"><span>{{ r }}</span></td>
+		  {% endfor %}
+		</div>
+	</div>
 </div>
 {% endblock %}
